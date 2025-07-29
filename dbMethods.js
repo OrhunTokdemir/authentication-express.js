938165c796a874e05ec8067a1868c2737451ab42
OrhunTokdemir <148989093+OrhunTokdemir@users.noreply.github.com>
2025-07-29 15:38:03 +0300

    routes and database
    #created the register route and the required insert and check methods for the
    database to use in the route

--- dbMethods.js
+++ dbMethods.js
@@ -12,3 +12,42 @@ async function insertUserToDb(pool, username, email, password) {
         client.release();
     }
 }
+
+async function checkUsernameAndEmail(pool, username, email) {
+    const client = await pool.connect();
+    try {
+        const result = {
+            username: null,
+            email: null
+        };
+
+        // Check username
+        const usernameRes = await client.query(
+            'SELECT username FROM users WHERE username = $1 LIMIT 1',
+            [username]
+        );
+        if (usernameRes.rows.length > 0) {
+            result.username = username;
+        }
+
+        // Check email
+        const emailRes = await client.query(
+            'SELECT email FROM users WHERE email = $1 LIMIT 1',
+            [email]
+        );
+        if (emailRes.rows.length > 0) {
+            result.email = email;
+        }
+
+        return result;
+    } catch (error) {
+        console.error('Error checking username and email:', error);
+        throw error;
+    } finally {
+        client.release();
+    }
+}
+module.exports = {
+    insertUserToDb,
+    checkUsernameAndEmail
+};
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { setupDatabase, pool } = require('./db');
+const { insertUserToDb, checkUsernameAndEmail } = require('./dbMethods');
 const app = express()
 const port = 3000
 
@@ -26,9 +27,25 @@ app.post('/login', (req, res) => {
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
     const {username, email, password}=req.body;
     if (username==null || email==null || password==null) {
         return res.status(400).send('Username, email and password are required!');
     }
+    try {
+        const result = await checkUsernameAndEmail(pool, username, email);
+        if (result.username && result.email) {
+            return res.status(409).send('Username and email already exist');
+        } else if (result.username) {
+            return res.status(409).send('Username already exists');
+        } else if (result.email) {
+            return res.status(409).send('Email already exists');
+        }
 
+        const newUser = await insertUserToDb(pool, username, email, password);
+        return res.status(201).json(newUser);
+    } catch (error) {
+        console.error('Error registering user:', error);
+        res.status(500).send('Internal server error');
+    }
+});
