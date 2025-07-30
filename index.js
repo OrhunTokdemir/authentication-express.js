731e5d97a7c24eba76bca3a05ee0497a7231caab
OrhunTokdemir <148989093+OrhunTokdemir@users.noreply.github.com>
2025-07-30 10:08:24 +0300

    worked on tokenizing

--- index.js
+++ index.js
@@ -29,9 +29,11 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 app.post('/register', async (req, res) => {
     const {username, email, password}=req.body;
+    //checks if the required fields are provided
     if (username==null || email==null || password==null) {
         return res.status(400).send('Username, email and password are required!');
     }
+    //checks if the username or email is already recorded in the database
     try {
         const result = await checkUsernameAndEmail(pool, username, email);
         if (result.username && result.email) {
@@ -43,9 +45,41 @@ app.post('/register', async (req, res) => {
         }
 
         const newUser = await insertUserToDb(pool, username, email, password);
-        return res.status(201).json(newUser);
+        console.log('New user registered:', newUser);
+        return res.status(201).send('User registered successfully');
     } catch (error) {
         console.error('Error registering user:', error);
         res.status(500).send('Internal server error');
     }
+    
+});
+
+app.post('/register-admin', async (req, res) => {
+    const {username, email, password, adminKey}=req.body;
+    //checks if the required fields are provided
+    if (username==null || email==null || password==null) {
+        return res.status(400).send('Username, email and password are required!');
+    }
+    if (adminKey !== process.env.ADMIN_KEY) {
+        return res.status(403).send('Forbidden: Invalid admin key');
+    }
+    //checks if the username or email is already recorded in the database
+    try {
+        const result = await checkUsernameAndEmail(pool, username, email);
+        if (result.username && result.email) {
+            return res.status(409).send('Username and email already exist');
+        } else if (result.username) {
+            return res.status(409).send('Username already exists');
+        } else if (result.email) {
+            return res.status(409).send('Email already exists');
+        }
+
+        const newUser = await insertUserToDb(pool, username, email, password);
+        console.log('New user registered:', newUser);
+        return res.status(201).send('User registered successfully');
+    } catch (error) {
+        console.error('Error registering user:', error);
+        res.status(500).send('Internal server error');
+    }
+    
 });
---
+++ jwt.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+async function generateToken(payload, duration = '1h') {
+
+    const token = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: duration });
+    return token;
+}
+
+async function verifyToken(token) {
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        return decoded;
+    } catch (err) {
+        console.error('Error verifying JWT:', err);
+        return null;
+    }
+}
+module.exports = {
+    generateToken,
+    verifyToken
+}; 
